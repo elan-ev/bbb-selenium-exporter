b146ca7657ed7c302d303d537361ea95f34a581c
Markus Otto <zuntrax@infra.run>
2020-12-27 00:41:54 +0100

    bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="bbb-selenium-exporter",
-    version="0.1.0",
+    version="0.1.1",
     author="Markus Otto",
     author_email="markus.otto@infra.run",
     description="Export Prometheus metrics scraped from BigBlueButton servers using Selenium",
